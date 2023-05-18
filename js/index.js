a137690cba3ba43e5acfaa49987eebc36cfb225a
mwape-k <125279827+Mwape-Kurete@users.noreply.github.com>
2023-05-18 21:13:21 +0200

    homework completed

--- js/index.js
+++ js/index.js
@@ -1,1 +1,34 @@
 //Week 4 - Task 3 Homework 
+
+calculateOperation = () => {
+    let valueOne = document.getElementById("val1").value;
+    let valueTwo = document.getElementById("val2").value;
+
+    let operation = document.getElementById("operation").value;
+
+    let answer;
+
+    console.log(valueOne + " " + valueTwo + " " + operation);
+
+    if (operation === "plus") {
+
+        answer = (valueOne + valueTwo);
+
+        alert("the answer is: " + answer);
+
+    } else if (operation === "minus") {
+
+        answer = valueOne - valueTwo;
+        alert("the answer is: " + answer);
+
+    } else if (operation === "times") {
+
+        answer = valueOne * valueTwo;
+        alert("the answer is: " + answer);
+
+    } else {
+
+        answer = valueOne / valueTwo;
+        alert("the answer is: " + answer);
+    }
+}
